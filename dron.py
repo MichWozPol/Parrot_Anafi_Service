e58724609eb37cc3843d9c3234724a84eecf7fcc
Mateusz <matiwoz1@gmail.com>
2021-12-11 23:19:45 +0100

    send data to server

--- dron.py
+++ dron.py
@@ -1,12 +1,18 @@
-import time
 import olympe
 from stream import Stream
 import requests
+import threading
+import signal
+
+from olympe.messages.ardrone3.Piloting import TakeOff
 from olympe.messages.common.CommonState import BatteryStateChanged
-from olympe.messages.ardrone3.PilotingState import PositionChanged, GpsLocationChanged, AltitudeAboveGroundChanged
+from olympe.messages.ardrone3.PilotingState import AltitudeAboveGroundChanged, FlyingStateChanged
+
+
+olympe.log.update_config({"loggers": {"olympe": {"level": "ERROR"}}})
 
 DRONE_IP = "192.168.53.1"
-URL = "http://localhost:8000/api"
+URL = "http://localhost:8080/api"
 CONNECTION_URL = URL + "/connection"
 BATTERY_URL = URL + "/battery"
 POSITION_URL = URL + "/position"
@@ -15,64 +21,93 @@ battery = 0
 connection = False
 altitude = 0,
 
-battery_data = {'battery': battery}
-connection_data = {'connection': connection}
-position_data = {'altitude': altitude}
+stop_thread = False
 
-class FlightListener(olympe.EventListener):
-    
-    @olympe.listen_event(BatteryStateChanged())
-    def onBatteryChange(self, event, scheduler):
-        battery = event.args['percent']
-        requests.post(POSITION_URL, json=battery_data)
-    
-    @olympe.listen_event(AltitudeAboveGroundChanged())
-    def onBatteryChange(self, event, scheduler):
-        altitude = event.args['altitude']
-        print(position_data)
-    # @olympe.listen_event(connection_state())
-    # def onConnectionChange(self, event, scheduler):
-    #     connection = event.args
-    #     print("Connnection changed", connection)
-    #     # requests.post(CONNECTION_URL, json=connection_data)
-    
+
+def check_position(device):
+    return device.get_state(AltitudeAboveGroundChanged)['altitude']
+
+
+def check_battery(device):
+    return device.get_state(BatteryStateChanged)['percent']
+
+
+def check_connection(device):
+    return device.connection_state()
+
+
+def check_battery_thread(device):
+    global battery
+    global stop_thread
+
+    while True:
+        if stop_thread:
+            break
+        local_battery = check_battery(device)
+        if local_battery != battery:
+            battery = local_battery
+            requests.post(BATTERY_URL, json={'batteryCharge': battery})
+
+
+def check_position_thread(device):
+    global altitude
+    global stop_thread
+
+    while True:
+        if stop_thread:
+            break
+        local_position = check_position(device)
+        if local_position != altitude:
+            altitude = local_position
+            requests.post(POSITION_URL, json={'altitude': altitude})
+
+
+def keyboardInterruptHandler(signal, frame):
+    global connection
+
+    print(connection)
+    connection = False
+    response = requests.post(CONNECTION_URL, json={'connection': connection})
+    print(response.json())
+
+    exit(0)
+
+signal.signal(signal.SIGINT, keyboardInterruptHandler)
+
 
 if __name__ == '__main__':
-    print('Creating drone')
-    drone = olympe.Drone(DRONE_IP)
-    with FlightListener(drone):
-        try:
-            # print('Creating drone')
-            # drone = olympe.Drone(DRONE_IP)
-            time.sleep(2)
+
+    try:
+        drone = olympe.Drone(DRONE_IP)
+
+        connection = check_connection(drone)
+        requests.post(CONNECTION_URL, json={'connection': connection})
+
+        drone.connect()
+        connection = check_connection(drone)
+        requests.post(CONNECTION_URL, json={'connection': connection})
 
 
-            drone.connect()
-            print('Established connection')
-            time.sleep(2)
-
-            print(drone.get_state(GpsLocationChanged))
-            # print(drone.get_state(AltitudeAboveGroundChanged))
-
-            print('Creating stream')
-            stream = Stream(drone)
-            time.sleep(5)
+        assert drone(TakeOff()).wait().success()
 
 
-            # print('Starting stream')
-            # stream.start()
-            # time.sleep(5)
+        battery_thread = threading.Thread(target=check_battery_thread, args=(drone,))
+        position_thread = threading.Thread(target=check_position_thread, args=(drone,))
 
-            stream.fly()
+        battery_thread.start()
+        position_thread.start()
 
-            # print('Stopping stream')
-            # stream.stop()
-
-
-
-        finally:
-
-            time.sleep(5)
-
-            drone.disconnect()
+        while True:
+            stop = str(drone.get_state(FlyingStateChanged)["state"])
+            if stop == "FlyingStateChanged_State.landed":
+                print("STOPPING APP")
+                stop_thread = True
+                break
+        
+        battery_thread.join()
+        position_thread.join()
     
+    finally:
+        drone.disconnect()
+        connection = check_connection(drone)
+        requests.post(CONNECTION_URL, json={'connection': connection})
