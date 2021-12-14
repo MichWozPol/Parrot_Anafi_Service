c36a5f8ca307c57dec5e15b655b30763f2730d8f
Mateusz <matiwoz1@gmail.com>
2021-12-14 15:04:14 +0100

    add stream and drone location

--- dron.py
+++ dron.py
@@ -1,12 +1,13 @@
 import olympe
-from stream import Stream
+from stream import Streaming
 import requests
 import threading
 import signal
 
 from olympe.messages.ardrone3.Piloting import TakeOff
 from olympe.messages.common.CommonState import BatteryStateChanged
-from olympe.messages.ardrone3.PilotingState import AltitudeAboveGroundChanged, FlyingStateChanged
+from olympe.messages.ardrone3.PilotingState import AltitudeAboveGroundChanged, FlyingStateChanged, PositionChanged
+from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged
 
 
 olympe.log.update_config({"loggers": {"olympe": {"level": "ERROR"}}})
@@ -15,11 +16,17 @@ DRONE_IP = "192.168.53.1"
 URL = "http://localhost:8080/api"
 CONNECTION_URL = URL + "/connection"
 BATTERY_URL = URL + "/battery"
-POSITION_URL = URL + "/position"
+ALTITUDE_URL = URL + "/altitude"
+GPS_URL = URL + "/gpslocation"
+STREAM_URL = URL + "/stream"
+
+
 
 battery = 0
 connection = False
-altitude = 0,
+altitude = 0
+longitude = 0
+latitude = 0
 
 stop_thread = False
 
@@ -36,6 +43,10 @@ def check_connection(device):
     return device.connection_state()
 
 
+def check_gps(device):
+    return device.get_state(PositionChanged)
+
+
 def check_battery_thread(device):
     global battery
     global stop_thread
@@ -49,6 +60,20 @@ def check_battery_thread(device):
             requests.post(BATTERY_URL, json={'batteryCharge': battery})
 
 
+def check_gps_thread(device):
+    global latitude
+    global longitude
+    global stop_thread
+
+    while True:
+        if stop_thread:
+            break
+        local_gps = check_gps(device)
+        latitude = local_gps['latitude']
+        longitude = local_gps['longitude']
+        requests.post(GPS_URL, json={'latitude': latitude, 'longitude': longitude})
+
+
 def check_position_thread(device):
     global altitude
     global stop_thread
@@ -59,8 +84,7 @@ def check_position_thread(device):
         local_position = check_position(device)
         if local_position != altitude:
             altitude = local_position
-            requests.post(POSITION_URL, json={'altitude': altitude})
-
+            requests.post(ALTITUDE_URL, json={'altitude': altitude}) 
 
 def keyboardInterruptHandler(signal, frame):
     global connection
@@ -87,15 +111,21 @@ if __name__ == '__main__':
         connection = check_connection(drone)
         requests.post(CONNECTION_URL, json={'connection': connection})
 
+        drone(GPSFixStateChanged(_policy = 'wait'))
+
+        stream = Streaming(drone, STREAM_URL)
+        stream.start()
 
         assert drone(TakeOff()).wait().success()
 
 
         battery_thread = threading.Thread(target=check_battery_thread, args=(drone,))
         position_thread = threading.Thread(target=check_position_thread, args=(drone,))
+        gps_thread = threading.Thread(target=check_gps_thread, args=(drone,))
 
         battery_thread.start()
         position_thread.start()
+        gps_thread.start()
 
         while True:
             stop = str(drone.get_state(FlyingStateChanged)["state"])
@@ -104,8 +134,11 @@ if __name__ == '__main__':
                 stop_thread = True
                 break
         
+        stream.stop()
+
         battery_thread.join()
         position_thread.join()
+        gps_thread.join()
     
     finally:
         drone.disconnect()
--- stream.py
+++ stream.py
@@ -1,110 +1,32 @@
-import tempfile, os, csv, cv2
 import olympe
-from pathlib import Path
-from olympe.messages.ardrone3.Piloting import TakeOff, Landing
-import time
-from olympe.messages.ardrone3.PilotingState import PositionChanged, GpsLocationChanged, AltitudeAboveGroundChanged
+import requests
 
 
-class Stream:
-    def __init__(self, drone):
+class Streaming:
+    def __init__(self, drone, endpoint):
         self.drone = drone
-    
-        self.tempd = tempfile.mkdtemp(prefix="olympe_streaming_test_")
-        print("Olympe streaming example output dir: {}".format(self.tempd))
+        self.endpoint = endpoint
 
-        self.h264_frame_stats = []
-        self.h264_stats_file = open(os.path.join(self.tempd, 'h264_stats.csv'), 'w+')
-        self.h264_stats_writer = csv.DictWriter(self.h264_stats_file, ['fps', 'bitrate'])
-        self.h264_stats_writer.writeheader()
-
-        self.dirname = 'image'
-        Path(self.dirname).mkdir(parents=True, exist_ok=True)
-
-        self.frame = None
-        self.isSteaming = False
 
     def start(self):
-        
-        if not self.isSteaming:
+        # Setup your callback functions to do some live video processing
+        self.drone.streaming.set_callbacks(
+            h264_cb=self.h264_frame_cb
+        )
 
-            self.drone.streaming.set_output_files(
-            video=os.path.join(self.tempd, "streaming.mp4"),
-            metadata=os.path.join(self.tempd, "streaming_metadata.json"),
-            )   
-                # Here, we don't record the (huge) raw YUV video stream
-                # raw_data_file=os.path.join(self.tempd,'raw_data.bin'),
-                # raw_meta_file=os.path.join(self.tempd,'raw_metadata.json'),
-            
-
-            # Setup your callback functions to do some live video processing
-            self.drone.streaming.set_callbacks(
-                raw_cb=self.yuv_frame_cb #,
-                #h264_cb=self.h264_frame_cb
-            )
-
-            # Start video streaming
-            self.drone.streaming.start()
-
-            self.isSteaming = True
-        
-        else:
-            print('Stream has already started.')
+        # Start video streaming
+        self.drone.streaming.start()
 
 
     def stop(self):
+        self.drone.streaming.stop()
 
-        if self.isSteaming:
-            # Properly stop the video stream and disconnect
-            self.drone.streaming.stop()
-            # self.drone.disconnection()
-            self.h264_stats_file.close()
 
-            self.isSteaming = False
-        else:
-            print('Cannot stop stream that has not started')
 
-    def yuv_frame_cb(self, yuv_frame):
+    def h264_frame_cb(self, h264_frame):
         """
-        This function will be called by Olympe for each decoded YUV frame.
+        This function will be called by Olympe for each new h264 frame.
             :type yuv_frame: olympe.VideoFrame
         """
-        
-        # the VideoFrame.info() dictionary contains some useful informations
-        # such as the video resolution
-        info = yuv_frame.info()
-        height, width = info["yuv"]["height"], info["yuv"]["width"]
-
-        # convert pdraw YUV flag to OpenCV YUV flag
-        cv2_cvt_color_flag = {
-            olympe.PDRAW_YUV_FORMAT_I420: cv2.COLOR_YUV2BGR_I420,
-            olympe.PDRAW_YUV_FORMAT_NV12: cv2.COLOR_YUV2BGR_NV12,
-        }[info["yuv"]["format"]]
-
-        # yuv_frame.as_ndarray() is a 2D numpy array with the proper "shape"
-        # i.e (3 * height / 2, width) because it's a YUV I420 or NV12 frame
-
-        # Use OpenCV to convert the yuv frame to RGB
-        cv2frame = cv2.cvtColor(yuv_frame.as_ndarray(), cv2_cvt_color_flag)
-
-        is_success, image_buffer_array = cv2.imencode('.jpg', cv2frame)
-
-        if is_success:
-            self.frame = image_buffer_array.tobytes()
-
-
-        status = cv2.imwrite(os.path.join(self.dirname, 'test_image.jpg'), cv2frame)
-        
-        # Use OpenCV to show this frame
-        # cv2.imshow("Olympe Streaming Example", cv2frame)
-        # cv2.waitKey(1)  # please OpenCV for 1 ms...
-
-    def get_latest_frame(self):
-        return self.frame
-
-    def fly(self):
-        assert self.drone(TakeOff()).wait().success()
-        # print(self.drone.get_state(AltitudeAboveGroundChanged))
-        time.sleep(5)
-        # print(self.drone.get_state(AltitudeAboveGroundChanged))
-        assert self.drone(Landing()).wait().success()
+        frame_array = h264_frame.as_ndarray()
+        requests.post(url=self.endpoint, json={'stream': frame_array})
