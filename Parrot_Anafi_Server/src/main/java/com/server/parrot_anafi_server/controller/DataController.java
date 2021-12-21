2b9270ad694a1523e9878660d871c06c9c015a0c
Michał Sakowicz <michalsakowicz99@gmail.com>
2021-12-21 17:29:09 +0100

    deleted useless endpoints, added updated jar files, improved README

--- Parrot_Anafi_Server/src/main/java/com/server/parrot_anafi_server/controller/DataController.java
+++ Parrot_Anafi_Server/src/main/java/com/server/parrot_anafi_server/controller/DataController.java
@@ -2,20 +2,11 @@ package com.server.parrot_anafi_server.controller;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.jcodec.codecs.h264.H264Decoder;
-import org.jcodec.common.model.ColorSpace;
-import org.jcodec.common.model.Picture;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.Queue;
-import java.util.stream.Stream;
-import org.apache.commons.lang3.ArrayUtils;
 
 @RestController
 @CrossOrigin(origins="*")
@@ -31,9 +22,6 @@ public class DataController {
     @Getter
     @Setter
     private boolean connectedToDrone = false;
-    @Getter
-    @Setter
-    private Queue<Byte[]> videoStream = new LinkedList<Byte[]>();
     @Getter
     @Setter
     private Map<String, Double> GPSLocation = new HashMap<>() {{put("longitude", 0d); put("latitude", 0d);}};
@@ -70,13 +58,6 @@ public class DataController {
         return HttpStatus.OK;
     }
 
-    @PostMapping("/stream")
-    public HttpStatus readSteam(@RequestBody Map<String, Byte[]> stream) throws IOException {
-        Byte[] streamValue = stream.get("stream");
-        videoStream.add(streamValue);
-        return HttpStatus.OK;
-    }
-
     @GetMapping("/connection")
     public Map<String, Boolean> getConnection() {
         Map<String, Boolean> connection = new HashMap<String, Boolean>();
@@ -104,39 +85,5 @@ public class DataController {
         gpsLocation.put("latitude", getGPSLocation().get("latitude"));
         gpsLocation.put("longitude", getGPSLocation().get("longitude"));
         return gpsLocation;
-    }
-
-    @GetMapping("/stream")
-    public Map<String, Byte[]> getStream() {
-        Map<String, Byte[]> currentStream = new HashMap<>();
-        if (!videoStream.isEmpty()) {
-            currentStream.put("stream", videoStream.poll());
-        }
-        else {
-            currentStream.put("stream", null);
-        }
-        return currentStream;
-    }
-
-    private byte[][] decodeImage(Byte[] imageArray) {
-        ByteBuffer bb = ByteBuffer.wrap(ArrayUtils.toPrimitive(imageArray)); // Your frame data is stored in this buffer
-        H264Decoder decoder = new H264Decoder();
-        Picture out = Picture.create(1280, 720, ColorSpace.RGB); // Allocate output frame of max size
-        Picture real = decoder.decodeFrame(bb, out.getData());
-        return real.getData();
-    }
-
-    @GetMapping("/stream2")
-    public Map<String, Byte[]> getStream2() {
-        Map<String, Byte[]> currentStream = new HashMap<>();
-        if (!videoStream.isEmpty()) {
-            byte[][] currentBytesArray = decodeImage(videoStream.poll());
-            Byte[] streamArray = Stream.of(currentBytesArray).flatMap(Stream::of).toArray(Byte[]::new);
-            currentStream.put("stream", streamArray);
-        }
-        else {
-            currentStream.put("stream", null);
-        }
-        return currentStream;
     }
 }
